c436ca04778f76cba161ba6ddad07c3bfebb5555
Mate Csanad <38218165+csanadm@users.noreply.github.com>
2023-07-29 04:50:52 -0400

    cosmetics

--- coulcorrtest.cc
+++ coulcorrtest.cc
@@ -8,10 +8,10 @@ int main()
   double lambda = 0.8;
   for(double Q=0.001; Q<0.2; Q+=0.001)
   {
-	double FullCorrFunc = ccinstance->CorrFuncValue(alpha, R, Q);
-	double CoulombCorr = ccinstance->CoulCorrValue(alpha, R, Q);
+    double FullCorrFunc = ccinstance->CorrFuncValue(alpha, R, Q);
+    double CoulombCorr = ccinstance->CoulCorrValue(alpha, R, Q);
     double PureCorrFunc = 1 + lambda*(FullCorrFunc/CoulombCorr-1);
-	double FullCorrFuncLambda = 1 - lambda + lambda*FullCorrFunc;
+    double FullCorrFuncLambda = 1 - lambda + lambda*FullCorrFunc;
     cout << Q << "\t" << PureCorrFunc << "\t" << FullCorrFuncLambda << "\t" << CoulombCorr << endl;
   }
   delete ccinstance;
