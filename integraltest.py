d911855d49e67c3971a37841c423f48c8c277de4
Mate Csanad <38218165+csanadm@users.noreply.github.com>
2023-07-30 10:16:17 -0400

    Add files via upload

--- integraltest.py
+++ integraltest.py
@@ -6,6 +6,7 @@ import os.path
 from matplotlib.colors import LogNorm
 from matplotlib.ticker import FuncFormatter
 from mpl_toolkits.axes_grid1 import make_axes_locatable
+import matplotlib.gridspec as gridspec
 
 fmt = lambda x, pos: '{:.6f}'.format(x)
 
@@ -33,29 +34,29 @@ integral_matrix = df.pivot_table(index="NMaxIter", columns="LogTolerance", value
 nfunccalls_matrix = df.pivot_table(index="NMaxIter", columns="LogTolerance", values="NFuncCalls", aggfunc=np.mean)
 
 # Create the two-panel plot with overlapping y-axes
-fig, axs = plt.subplots(1, 2, figsize=(16, 8), gridspec_kw={'wspace': 0.0})
+fig, axs = plt.subplots(1, 2, figsize=(16, 8), gridspec_kw={'wspace': 0}) #gridspec_kw={'width_ratios': [0.8, 0.05]})
 
 # Plot for "Integral" with color bar on the left side
 im1 = axs[0].imshow(integral_matrix, cmap="viridis", origin="lower", aspect="auto", extent=[min(df["LogTolerance"]), max(df["LogTolerance"]), min(df["NMaxIter"]), max(df["NMaxIter"])])
-axs[0].set_title(r'$C(Q)-1$ for' "\n" r'$\lambda={0}$, $R={1}$ fm, $\alpha={2}$, $Q={3}$ GeV/c'.format(1.0, 6.0, 1.4, 0.20))
+axs[0].set_title(r'$C(Q)-1$ for' "\n" r'$\lambda={0}$, $R={1}$ fm, $\alpha={2}$, $Q={3}$ GeV/c'.format(1.0, 6.0, 1.4, 0.04))
 axs[0].set_xlabel(r"-log$_{10}$(tolerance)")
 axs[0].set_ylabel("# of max iterations")
-cbar1 = plt.colorbar(im1, ax=axs[0], location='left', format=FuncFormatter(fmt))
-cbar1.set_label("C(Q)-1", rotation=90, labelpad=20)  # Rotate the color bar label and increase padding
+cbar0 = plt.colorbar(im1, ax=axs[0], location='left', format=FuncFormatter(fmt))
+cbar0.set_label("C(Q)-1", rotation=90, labelpad=10)  # Rotate the color bar label and increase padding
 
 # Plot for "NFuncCalls" with logarithmic scale and color bar on the right side
 im2 = axs[1].imshow(nfunccalls_matrix, cmap="viridis", origin="lower", aspect="auto", extent=[min(df["LogTolerance"]), max(df["LogTolerance"]), min(df["NMaxIter"]), max(df["NMaxIter"])], norm=LogNorm())
-#axs[1].set_title()
 axs[1].yaxis.tick_right()
-axs[1].yaxis.set_label_position("right")
-axs[0].set_ylabel("# of max iterations")
-axs[1].set_title(r'# of function calls for' "\n" r'$\lambda={0}$, $R={1}$ fm, $\alpha={2}$, $Q={3}$ GeV/c'.format(1.0, 6.0, 1.4, 0.20))
+axs[1].set_title(r'# of function calls for' "\n" r'$\lambda={0}$, $R={1}$ fm, $\alpha={2}$, $Q={3}$ GeV/c'.format(1.0, 6.0, 1.4, 0.04))
 axs[1].set_xlabel(r"-log$_{10}$(tolerance)")
-cbar2 = plt.colorbar(im2, ax=axs[1])
-cbar2.set_label("# of function calls", rotation=270, labelpad=10)  # Rotate the color bar label and increase padding
+axs[1].set_ylabel("# of max iterations")  # Adjust labelpad to position the title on the right
+axs[1].yaxis.set_label_position("right")  # Set the y-axis title to appear on the right side
+cbar1 = plt.colorbar(im2, ax=axs[1], pad=0.11)
+cbar1.set_label("# of function calls", rotation=270, labelpad=15) 
+
+plt.subplots_adjust(right=0.85)  # You can adjust the value (0.85 in this case) to create enough space between the y-axis label and color bar
 
 plt.tight_layout()
-plt.subplots_adjust(wspace=0.01)  # Adjust the space between the plots
 
 pngfile = "integraltest.png"
 if(len(sys.argv)>2): pngfile=sys.argv[2]
