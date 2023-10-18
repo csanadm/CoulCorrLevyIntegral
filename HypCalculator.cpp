70ae6bc8ff6893d12d90fc584271ce243cdbcb20
Mate Csanad <38218165+csanadm@users.noreply.github.com>
2023-10-18 10:30:10 +0200

    Update HypCalculator.cpp
    Adding option for Fminus type of functions, renaming F1_F2_F3 to Fminus

--- HypCalculator.cpp
+++ HypCalculator.cpp
@@ -24,8 +24,22 @@ HypCalculator::HypCalculator()
   B = complex_zero;
   C = complex_zero;
 
+  sinabc_minus     = complex_zero; 
+  sinab_minus      = complex_zero;
+  Gamma_c_minus    = complex_zero;
+  Gamma_ab1c_minus = complex_zero;
+  Gamma_ca_minus   = complex_zero;
+  Gamma_cb_minus   = complex_zero;
+  Gamma_a_minus    = complex_zero;
+  Gamma_b_minus    = complex_zero;
+  Gamma_c1ab_minus = complex_zero;
+  A_minus = complex_zero; 
+  B_minus = complex_zero;
+  C_minus = complex_zero;
+
   pieta_cth_pieta = 0.;
   tilde_s0eta = complex_zero;
+  tilde_s0eta_c2 = complex_zero;
   tilde_nu0 = complex_zero;
   sinhpieta_pieta = complex_zero;
 
@@ -79,6 +93,9 @@ void HypCalculator::initialize_eta(const double _eta)
   complex<double> a(0., eta);
   complex<double> b(1., eta);
   complex<double> c(1., 0.);
+  complex<double> a_minus(0., -eta);
+  complex<double> b_minus(1., -eta);
+  complex<double> c_minus(2., 0.);
   
   sinabc     = sin(M_PI * (c-a-b));  
   sinab      = sin(M_PI * (a-b));  
@@ -92,6 +109,18 @@ void HypCalculator::initialize_eta(const double _eta)
   A = a;
   B = b;
   C = c;
+  sinabc_minus     = sin(M_PI * (c_minus-a_minus-b_minus));  
+  sinab_minus      = sin(M_PI * (a_minus-b_minus));  
+  Gamma_c_minus    = Gamma(c_minus);
+  Gamma_ab1c_minus = Gamma(a_minus+b_minus+1.-c_minus); 
+  Gamma_ca_minus   = Gamma(c_minus-a_minus); 
+  Gamma_cb_minus   = Gamma(c_minus-b_minus); 
+  Gamma_a_minus    = Gamma(a_minus); 
+  Gamma_b_minus    = Gamma(b_minus); 
+  Gamma_c1ab_minus = Gamma(c_minus+1.-a_minus-b_minus); 
+  A_minus = a_minus;
+  B_minus = b_minus;
+  C_minus = c_minus;
   
   if(eta == 0.)
   {
@@ -108,6 +137,13 @@ void HypCalculator::initialize_eta(const double _eta)
   tilde_s0eta = s0;
   complex<double> ieta1(1., eta);
   tilde_s0eta += 2. * eta * digamma(ieta1);
+
+  complex<double> I(0.,1.);
+  complex<double> s0_c2(1. - pieta_cth_pieta, 2. * eta * euler_gamma - eta);
+  tilde_s0eta_c2 = s0_c2;
+  complex<double> mieta1(1., -eta);
+  tilde_s0eta_c2 += 2. * I * eta * digamma(mieta1);
+
   tilde_nu0 = -1. * eta;
   
   is_eta_initialized = true;
@@ -135,6 +171,27 @@ complex<double> HypCalculator::Gauss_2F1_series0(const double z)
   return result;
 }
 
+complex<double> HypCalculator::Gauss_2F1_series0_minus(const double z)
+{
+  complex<double> result(-9999., 0);
+  if(is_eta_initialized)
+  {
+    if(abs(z) < 0.9999)
+    {
+      complex<double> n(0., 0.);
+      complex<double> term(1., 0);
+      result = term;
+      while(abs(term) > EPSILON)
+      {
+        term = term * z / (n + 1.) * (A_minus + n) * (B_minus + n) / (C_minus + n);
+        result += term;
+        n += 1. ;
+      }
+    }
+  }
+  return result;
+}
+
 complex<double> HypCalculator::Gauss_2F1_1mZ_noint(const double z)
 {
   complex<double> result(-9999., 0);
@@ -145,6 +202,26 @@ complex<double> HypCalculator::Gauss_2F1_1mZ_noint(const double z)
       if(abs(sinabc) > EPSILON_ABC)
         result = M_PI / sinabc * Gamma_c * ( Gauss_2F1_series0(A, B, A+B+1.-C, 1.-z) / Gamma_ab1c / Gamma_ca / Gamma_cb
                                             -Gauss_2F1_series0(C-A, C-B, C+1.-A-B, 1.-z) / Gamma_a / Gamma_b / Gamma_c1ab / pow(1.-z, A+B-C));
+    }
+  }
+  return result;
+}
+
+complex<double> HypCalculator::Gauss_2F1_1mZ_noint_minus(const double z, const bool is_abovecut)
+{
+  complex<double> result(-9999., 0);
+  complex<double> aux1(1.-z, 0.);
+  complex<double> aux2(z-1., 0.);
+  complex<double> aux = (is_abovecut ? aux2 : aux1);
+  if(is_eta_initialized)
+  {
+    if(abs(1. - z) < 0.9999)
+    {
+      if(abs(sinabc_minus) > EPSILON_ABC)
+        result = M_PI / sinabc_minus * Gamma_c_minus * ( Gauss_2F1_series0(A_minus, B_minus, A_minus+B_minus+1.-C_minus, 1.-z) / Gamma_ab1c_minus / Gamma_ca_minus
+                                           / Gamma_cb_minus
+                                            -Gauss_2F1_series0(C_minus-A_minus, C_minus-B_minus, C_minus+1.-A_minus-B_minus, 1.-z) 
+                          / Gamma_a_minus / Gamma_b_minus / Gamma_c1ab_minus / pow( ( is_abovecut ? -1. : 1. ) * aux , A_minus+B_minus-C_minus));
     }
   }
   return result;
@@ -169,7 +246,7 @@ complex<double> HypCalculator::Gauss_2F1_1perZ_noint(const double z)
   return result;
 }
 
-complex<double> HypCalculator::Gauss_2F1_c_1(const double z)
+complex<double> HypCalculator::Gauss_2F1_1perz_spec_c1(const double z)
 {
   complex<double> result(-9999., 0.);
   complex<double> I(0.,1.);
@@ -196,7 +273,36 @@ complex<double> HypCalculator::Gauss_2F1_c_1(const double z)
   return result;
 }
 
-complex<double> HypCalculator::F1_F2_F3(const double x)
+complex<double> HypCalculator::Gauss_2F1_1perz_spec_c2(const double z, const bool is_abovecut)
+{
+  complex<double> result(-9999., 0.);
+  complex<double> I(0.,1.);
+  complex<double> aux1(-z, 0.);
+  complex<double> aux2(z, 0.);
+  complex<double> aux = (is_abovecut ? aux2 : aux1);
+  if(is_eta_initialized)
+  {
+    if(abs(1./z) < 0.9999)
+    {
+      complex<double> s = - I * eta * log( ( is_abovecut ? -1. : 1. ) * aux) + tilde_s0eta_c2; // ide beírtam egy mínuszt
+      complex<double> nu = 1. / z;
+      double n = 0.;
+      complex<double> sum = nu * s;
+      while(abs(nu * s) > EPSILON)
+//      while(abs(sum) > EPSILON)
+      {
+        nu = nu * (1. - (I * eta + 1.) / (n + 1.)) * (1. - (I * eta + 1.)/(n + 2.)) / z;
+        s +=  I * eta /(n + 1. - I * eta) + I *eta / (n - I * eta) - I * eta * (2. * n + 3.)/(n * n + 3. * n + 2.);
+        sum += (nu * s);
+        n += 1.;
+      }   
+      result = sinhpieta_pieta * pow( ( is_abovecut ? -1. : 1. ) * aux, I * eta) * ( 1. / (1. + I*eta) + sum);
+    }
+  }
+  return result;
+}
+
+complex<double> HypCalculator::Fplus(const double x) // -> Fplus ???
 {
   complex<double> result(-9999., 0.);
   if(x >= 0. && is_eta_initialized)
@@ -206,9 +312,25 @@ complex<double> HypCalculator::F1_F2_F3(const double x)
     else if(x <= 1.6)
       result = Gauss_2F1_1mZ_noint(x);
     else
-      result = Gauss_2F1_c_1(x);
+      result = Gauss_2F1_1perz_spec_c1(x);
   }
   return result;
 
 }
 
+complex<double> HypCalculator::Fminus(const double x, const bool is_abovecut) // -> Fminus, 
+{
+  complex<double> result(-9999., 0.);
+  if(x >= 0. && is_eta_initialized)
+  {
+    if(x <= 0.6)
+      result = Gauss_2F1_series0_minus(x);
+    else if(x <= 1.6)
+      result = Gauss_2F1_1mZ_noint_minus(x, is_abovecut);
+    else
+      result = Gauss_2F1_1perz_spec_c2(x, is_abovecut);
+  }
+  complex<double> I(0.,1.);
+  return (1. + I * eta) * result;
+
+}
